96124394066b4be23c08709e9b95635c7174849b
Amit Kumar <ssamitkmr@gmail.com>
2018-06-18 14:26:43 +0530

    diameter in binary search tree

---
+++ Tree/diameter_binary_tree.py
@@ -0,0 +1,101 @@
+"""
+
+Given a Binary Tree, find diameter of it.  The diameter of a tree is the number of nodes on the longest path between two leaves in the tree. The diagram below shows two trees each with diameter nine, the leaves that form the ends of a longest path are shaded (note that there is more than one path in each tree of length nine, but no path longer than nine nodes).
+
+
+
+Input:
+The task is to complete the method which takes one argument, root of Binary Tree. The struct Node has a data part which stores the data, pointer to left child and pointer to right child.
+There are multiple test cases. For each test case, this method will be called individually.
+
+Output:
+The function should return diameter of binary tree.
+
+Constraints:
+1 <=T<= 30
+1 <=Number of nodes<= 100
+1 <=Data of a node<= 1000
+
+Example:
+Input:
+2
+2
+1 2 L 1 3 R
+4
+10 20 L 10 30 R 20 40 L 20 60 R
+
+Output:
+3
+4
+"""
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+class Tree:
+    def createNode(self, data):
+        return Node(data)
+    def insert(self, node, data, ch):
+        if node is None:
+            return self.createNode(data)
+        if (ch == 'L'):
+            node.left = self.insert(node.left, data, ch)
+            return node.left
+        else:
+            node.right = self.insert(node.right, data, ch)
+            return node.right
+    def search(self, lis, data):
+        # if root is None or root is the search data.
+        for i in lis:
+            if i.data == data:
+                return i
+
+
+
+''' Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above. '''
+
+'''
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
+# your task is to complete this function
+# function should return the diameter of the tree
+
+def height(root):
+    if root == None:
+        return 0
+    return 1 + max(height(root.left),height(root.right))
+
+def diameter(root):
+    # Code here
+    if root == None:
+        return 0
+    return max(diameter(root.left),diameter(root.right),1+height(root.left)+height(root.right))
+
+# Driver Program
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = input().strip().split()
+        if n ==0:
+            print(0)
+            continue
+        tree = Tree()
+        lis=[]
+        root = None
+        root = tree.insert(root, int(arr[0]), 'L')
+        lis.append(root)
+        k=0
+        for j in range(n):
+            ptr = None
+            ptr = tree.search(lis, int(arr[k]))
+            lis.append(tree.insert(ptr, int(arr[k+1]), arr[k+2]))
+            k+=3
+        print(diameter(root))
