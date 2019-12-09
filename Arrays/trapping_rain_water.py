0232501d6e3a07a67745538116141ff7ec424611
Amit Kumar <ssamitkmr@gmail.com>
2019-12-09 11:38:05 +0530

    add new method to solve rain watar problem

--- Arrays/trapping_rain_water.py
+++ Arrays/trapping_rain_water.py
@@ -12,8 +12,6 @@
 
 # Below is another example.
 
-
-
 # Input:
 # The first line of input contains an integer T denoting the number of test cases. The description of T test cases follows.
 # Each test case contains an integer N followed by N numbers to be stored in array.
@@ -38,33 +36,76 @@
 # 10
 # 0
 
-def trapping_rain_water(arr,n):
+import unittest
+
+
+def trapping_rain_water_array(arr, n):
+    left_high = []
+    right_high = []
+
+    left_max = 0
+    right_max = 0
+    for i in range(len(arr)):
+        left_high.append(left_max)
+        right_high.append(right_max)
+
+        if left_max < arr[i]:
+            left_max = arr[i]
+
+        if right_max < arr[n - 1 - i]:
+            right_max = arr[n - 1 - i]
+
+    trapped_water = 0
+
+    for i in range(len(arr)):
+        trapped_water += max(0, min(left_high[i], right_high[i]) - arr[i])
+
+    return trapped_water
+
+
+def trapping_rain_water(arr, n):
     stack = []
 
     water_quantity = 0
     columns_area = 0
-    for i in range(0,n):
+    for i in range(0, n):
         if len(stack) == 0:
             stack.append(i)
             continue
-        
-        while(len(stack)>1 and arr[stack[len(stack)-1]]<arr[i]):
-            columns_area = columns_area + arr[stack[len(stack)-1]]
-            del(stack[len(stack)-1])
-        if len(stack) == 1 and arr[stack[len(stack)-1]]<arr[i]:
-            water_quantity = water_quantity + (i-stack[0]- 1)*arr[stack[0]]
-            del(stack[0])
+
+        while (len(stack) > 1 and arr[stack[len(stack) - 1]] < arr[i]):
+            columns_area = columns_area + arr[stack[len(stack) - 1]]
+            del (stack[len(stack) - 1])
+        if len(stack) == 1 and arr[stack[len(stack) - 1]] < arr[i]:
+            water_quantity = water_quantity + (i - stack[0] -
+                                               1) * arr[stack[0]]
+            del (stack[0])
         stack.append(i)
-    
-    for i in range(0,len(stack)-1):
-        water_quantity = water_quantity + arr[stack[len(stack)-1-i]] * (stack[len(stack)-1-i]-stack[len(stack)-2-i]-1)
+
+    for i in range(0, len(stack) - 1):
+        water_quantity = water_quantity + arr[stack[len(stack) - 1 - i]] * (
+            stack[len(stack) - 1 - i] - stack[len(stack) - 2 - i] - 1)
 
     return water_quantity - columns_area
 
-t = int(input())
 
-for i in range(0,t):
-    n = int(input())
-    arr = [int(x) for x in input().strip().split(" ")]
-    print(trapping_rain_water(arr,n))
+def sys_input():
+    t = int(input())
 
+    for i in range(0, t):
+        n = int(input())
+        arr = [int(x) for x in input().strip().split(" ")]
+        print(trapping_rain_water(arr, n))
+
+
+class TestCase(unittest.TestCase):
+    def test_rainwater(self):
+        dataT = [([7, 4, 0, 9], 10), ([6, 9, 9], 0), ([2, 0, 2], 2)]
+
+        for arr, exp in dataT:
+            actual = trapping_rain_water_array(arr, 0)
+            self.assertEqual(actual, exp)
+
+
+if __name__ == "__main__":
+    unittest.main()
